6e536cde68e5dcf27aa3386f7cfab03eaf2ebcd5
Gustavo Marques <gustavomsf21@gmail.com>
2024-03-22 19:50:45 -0300

    subindo projeto

---
+++ App.js
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+import Contador from './src/Contador'
+
+function App(){
+  return(
+    <View>
+      <Contador />
+    </View>
+  )
+}
+
+export default App;
